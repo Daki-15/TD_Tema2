a9ef22aadc0ec477514f20b819df373b9746e505
Daki15 <darjanpaunovic@gmail.com>
2022-10-25 21:35:43 +0200

    add error state for all state

--- at.c
+++ at.c
@@ -8,6 +8,7 @@ DATA data;
 uint8_t parse(char ch){
    //o variabila care tine minte starea curenta a automatului
    static uint8_t current_state = 0;
+
    switch (current_state) {
     case 0:{
         if(ch == 13) // CR
@@ -15,7 +16,7 @@ uint8_t parse(char ch){
     } break; 
 
     case 1:{
-        if(ch == 10) // LF {
+        if(ch == 10) // LF
             current_state = 2;
         else{
            // state error
@@ -52,38 +53,69 @@ uint8_t parse(char ch){
     case 21:{
         if(ch == 10) // LF
             current_state = 22;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break; 
 
     case 22:{
         if(ch == 13) // CR
             current_state = 23;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 23:{
         if(ch == 10) // LF
             current_state = 24;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break; 
 
     case 24:{
-        if(ch == 79) // 'O'
-            current_state = 30;
-        if(ch == 69) // 'E'
-            current_state = 40;
+        switch (ch) {
+           case 'O': {
+                current_state = 30;
+            } break;
+            case 'E': {
+               current_state = 40;
+            } break;
+            default: {
+               // error state 
+               current_state = -1; //return to 0(start) state
+            }
     } break; 
 
     case 30:{   
         if(ch == 75) // 'K'
             current_state = 31;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 31:{   
         if(ch == 13) // CR
             current_state = 32;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 32:{   
         if(ch == 10) // LF
             current_state = 33;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 33:{   
@@ -93,32 +125,56 @@ uint8_t parse(char ch){
     case 40:{   
         if(ch == 82) // 'R'
             current_state = 41;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 41:{   
         if(ch == 82) // 'R'
             current_state = 42;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 42:{   
         if(ch == 79) // 'O'
             current_state = 43;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 43:{   
         if(ch == 82) // 'R'
             current_state = 44;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case 44:{   
         if(ch == 13) // CR
             current_state = 32;
+        else{
+            // state error
+           current_state = -1;
+        }
     } break;
 
     case -1:{
         current_state = 0;
         return 0; // 0 - error state machine
     } break;
-
+    
+    default:{
+        // error state 
+        return 0; // 0 - error state machine
+    }
     }
 }
