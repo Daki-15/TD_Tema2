e8e63877d6ac27e7eeaaa838cc5a45ed3e2974c5
Daki15 <darjanpaunovic@gmail.com>
2022-11-04 12:24:05 +0100

    Fix the print and save struct data

--- at.c
+++ at.c
@@ -3,234 +3,237 @@
 // 2 - work in progress
 #include "at.h"
 #include <string.h>
+#include <stdlib.h>
 
 DATA data; 
-#define STR_SIZE 100
+#define ERROR_STATE 10
 
 uint8_t parse(char ch){
    //o variabila care tine minte starea curenta a automatului
    static uint8_t current_state = 0;
-   data.line_count= 0;
-   static int _count=0;
+   static int _count;
 
    switch (current_state) {
     case 0:{
+        data.line_count= 0;
+        data.ok_error = 0;
+        _count = 0;
+        memset(data.strings, 0, sizeof(data.strings)/sizeof(data.strings[0])); // Restart string array
         if(ch == 13){ // CR
-            strcpy(data.strings[_count++], "<CH>\0");
+            strcpy(data.strings[_count++], "<CH>");
             current_state = 1;
+        } else{
+            current_state = ERROR_STATE;
         }
     } break; 
 
     case 1:{
         if(ch == 10){ // LF
-            strcpy(data.strings[_count++], "<LF>\0");
+            strcpy(data.strings[_count++], "<LF>");
             data.line_count++;
             current_state = 2;
         }
         else{
            // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         } 
     } break;
 
     case 2: {
          switch (ch) {
-           case '+': {
-                strcpy(data.strings[_count++], "+\0");
+           case 43: { // ' + '
+                strcpy(data.strings[_count++], "+");
                 current_state = 20;
 
             } break;
-            case 'O': {
-               strcpy(data.strings[_count++], "O\0");
+            case 79: { //' O '
+               strcpy(data.strings[_count++], "O");
                current_state = 30;
             } break; 
-            case 'E': {
-               strcpy(data.strings[_count++], "E\0");
+            case 69: { // ' E '
+               strcpy(data.strings[_count++], "E");
                current_state = 40;
             } break;
             default: {
                // error state 
-               current_state = 10; //return to 0(start) state
+               current_state = ERROR_STATE; //return to 0(start) state
             }
          }
     }break; 
 
     case 20:{
-        if(ch == 13){ //CR
-            strcpy(data.strings[_count++], "<CH>\0");
-            current_state = 21;
+        if(ch != 13) { // ch != CR or LF
+            strncat(data.strings[_count++], &ch, 1);
+            current_state = 20;
         }
-        else{// ch != CR or LF
-           strncat(data.strings[_count++], &ch, 1);
-           current_state = 20;
+        else{ // CR
+            strcpy(data.strings[_count++], "<CH>");
+            current_state = 21;
         }
     } break;
 
     case 21:{
         if(ch == 10){ // LF
-            strcpy(data.strings[_count++], "<LF>\0");
+            strcpy(data.strings[_count++], "<LF>");
             data.line_count++;
             current_state = 22;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break; 
 
     case 22:{
         switch(ch){
             case 13:{ //CR
-                strcpy(data.strings[_count++], "<CH>\0");
+                strcpy(data.strings[_count++], "<CH>");
                 current_state = 23;
 
             } break;
-            case 43:{ // '+'
-                strcpy(data.strings[_count++], "+\0");
+            case 43:{ // ' + '
+                strcpy(data.strings[_count++], "+");
                 current_state = 20;
 
             } break;
             default:
                 // state error
-                current_state = 10;
+                current_state = ERROR_STATE;
         }
     } break;
 
     case 23:{
         if(ch == 10){ // LF
-            strcpy(data.strings[_count++], "<LF>\0");
+            strcpy(data.strings[_count++], "<LF>");
             data.line_count++;
             current_state = 24;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break; 
 
     case 24:{
         switch (ch) {
-           case 'O': {
-                strcpy(data.strings[_count++], "O\0");
+           case 79: { // ' O '
+                strcpy(data.strings[_count++], "O");
                 current_state = 30;
 
             } break;
-            case 'E': {
-               strcpy(data.strings[_count++], "E\0");
+            case 69: { // ' E '
+               strcpy(data.strings[_count++], "E");
                current_state = 40;
 
             } break;
             default: {
                // error state 
-               current_state = 10; //return to 0(start) state
+               current_state = ERROR_STATE; //return to 0(start) state
             }
         }
     } break; 
 
     case 30:{   
-        if(ch == 75){ // 'K'
-            data.line_count++;
-            strcpy(data.strings[_count++], "K\0");
+        if(ch == 75){ // ' K '
+            strcpy(data.strings[_count++], "K");
             current_state = 31;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 31:{   
         if(ch == 13){ // CR
-            strcpy(data.strings[_count++], "<CH>\0");
+            strcpy(data.strings[_count++], "<CH>");
             current_state = 32;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 32:{   
         if(ch == 10){ // LF
-            strcpy(data.strings[_count++], "<LF>\0");
+            strcpy(data.strings[_count++], "<LF>");
             data.line_count++;
             current_state = 33;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 33:{   
+        current_state = 0;
         data.ok_error = 1;
-        return 1;  // 1 - ok state machine
+        return data.ok_error;  // 1 - ok state machine
     } break;
 
     case 40:{   
-        if(ch == 82){ // 'R'
-            strcpy(data.strings[_count++], "R\0");
+        if(ch == 82){ // ' R '
+            strcpy(data.strings[_count++], "R");
             current_state = 41;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 41:{   
-        if(ch == 82){ // 'R'
-            strcpy(data.strings[_count++], "R\0");
+        if(ch == 82){ // ' R '
+            strcpy(data.strings[_count++], "R");
             current_state = 42;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 42:{   
-        if(ch == 79){ // 'O'
-            strcpy(data.strings[_count++], "O\0");
+        if(ch == 79){ // ' O '
+            strcpy(data.strings[_count++], "O");
             current_state = 43;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 43:{   
-        if(ch == 82){ // 'R'
-            strcpy(data.strings[_count++], "R\0");
+        if(ch == 82){ // ' R '
+            strcpy(data.strings[_count++], "R");
             current_state = 44;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
     case 44:{   
         if(ch == 13){ // CR
-            strcpy(data.strings[_count++], "<CH>\0");
+            strcpy(data.strings[_count++], "<CH>");
             current_state = 32;
         }
         else{
             // state error
-           current_state = 10;
+           current_state = ERROR_STATE;
         }
     } break;
 
-    case 10:{
-        _count=0;
-        current_state = 0;
-        return 0; // 0 - error state machine
+    case ERROR_STATE:{
+        data.ok_error = 0;
+        return data.ok_error; // 0 - error state machine
     } break;
 
     default:{
-        // error state 
-        _count=0;
         current_state = 0;
         return 0; // 0 - error state machine
     }
--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print(){
-   int i=0;
+void print(int N){
    
-   while (i<10)
+   for(int i = 0; i<N; i++ )
    {
-      printf("%s\n", data.strings[i]);
-      i++;
+      if(!strcmp(data.strings[i], "<LF>")){
+         printf("\n");
+      } 
+      printf("%s", data.strings[i-1]);
    }
+   //printf("\nSIZE = %d", sizeof(data.strings)/sizeof(data.strings[0]));
     
 }
 
 int main(int argc, char **argv) {
    FILE *f;
-   int result;
+   int result, N=0;
    char ch;
 
    f = fopen(argv[1], "rb");
@@ -30,24 +32,17 @@ int main(int argc, char **argv) {
       ch = fgetc(f);
       //printf("%c", ch);
       result = parse(ch);
+      N++;
 
       if(result == 0) {
          printf("\nError on changing state in automata...\n");
          exit(2);
       }
+      if(data.ok_error == 1){
+         printf("OK");
+         print(N);
+      }
    }
-
-   if(result == 1) {
-      // protocol ok
-      printf("OK\n");
-      print();
-      
-      //printf("\n%lu", (unsigned long)data.line_count);
-      //printf("\n%u", (unsigned)data.ok_error);
-      // print result (struct...)
-      // print(data); <- functie 
-   }
-
    /*
    if(result == 2) {
       // work in progress... read more chars 
